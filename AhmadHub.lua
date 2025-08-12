ff21a1cb3d594a5703a58e8cc5dfbeffe0159b81
AhmadV099 <jazztv022@gmail.com>
2025-08-13 04:18:03 +0800

    Update AhmadHub.lua

--- AhmadHub.lua
+++ AhmadHub.lua
@@ -1,3 +1,1 @@
 loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/CbEfj56VhWuaVwr6/raw", true))()
-ID="7292f569-2fbd-4d47-820c-86f02171aa3e";
-loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
